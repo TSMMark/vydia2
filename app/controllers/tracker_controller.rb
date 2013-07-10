592d85230c4c12349382f86c76af8055a4617d7a
Mark Allen <marka@trendsettermarketing.net>
2013-07-10 17:23:58 -0400

    no dupes

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -1,41 +1,70 @@
 class TrackerController < ApplicationController
-
-  before_filter :default_format
-  def default_format
-    request.format = "json" unless params[:format]
-  end
-
   respond_to :html, :json
 
-  def pixel
-    @video      = Video.find params[:id]
-    @network    = Network.find params[:network_id]
+  before_filter :default_format
+  before_filter :load_resources
+  before_filter :get_ip
+  before_filter :existing_impression
 
-    ip_address  = request.remote_ip
+  def impression
+    if @impression.blank?
+      @video      = Video.find params[:id]
+      @network    = Network.find params[:network_id]
 
-    if @video && @network then
-      @impression = Impression.new do |imp|
-        imp.video_id    = @video.id
-        imp.network_id  = @network.id
-        imp.ip_address  = ip_address
-        imp.request_url = request.env['REQUEST_URI']
-        imp.referer     = request.env['HTTP_REFERER']
-        imp.user_agent  = request.env['HTTP_USER_AGENT']
-      end 
-      
-      @impression.save!
+      if @video && @network then
+        @impression = Impression.new do |imp|
+          imp.video_id    = @video.id
+          imp.network_id  = @network.id
+          imp.ip_address  = @ip_address
+          imp.request_url = request.env['REQUEST_URI']
+          imp.referer     = request.env['HTTP_REFERER']
+          imp.user_agent  = request.env['HTTP_USER_AGENT']
+        end 
+        
+        @impression.save!
+      end
     end
     
     render_blank_pixel
   end
 
   def view
-    @video    = Video.find params[:id]
-    @network  = Network.find params[:network_id]
+    unless @impression.blank?
 
-    respond_with @video
+    end
 
-
+    render_blank_pixel
   end
+
+
+
+
+  protected
+
+  def default_format
+    request.format = "json" unless params[:format]
+  end
+  
+  def get_ip
+    @ip_address = request.remote_ip
+  end
+
+  def load_resources
+    @video      = Video.find params[:id]
+    @network    = Network.find params[:network_id]
+  end
+
+  def existing_impression
+    unless @video.blank? || @network.blank?
+      @impression = Impression.where(
+        video_id:   @video.id,
+        network_id: @network.id,
+        ip_address: @ip_address
+      ).limit(1).last
+    else
+      @impression = nil
+    end
+  end
+
 
 end
