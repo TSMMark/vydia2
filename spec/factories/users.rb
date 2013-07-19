c9dd0f145009fecc5dba6f5e0744beb1fa732281
TSMMark <marka@trendsettermarketing.net>
2013-07-19 12:45:45 -0400

    User spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,19 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence(:name) { |n| "Example Name #{n}" }
+  sequence(:email) { |n| "foo#{n}@trendsettermarketing.net" }
+  sequence(:bad_email) { |n| "foo#{n}@example.com" }
+
   factory :user do
+    email
+    password  "foobar123"
+    password_confirmation { "#{password}" }
   end
+  factory :user_default, parent: :user do
+  end
+  factory :user_non_trendsetter, parent: :user do
+    email { FactoryGirl.generate :bad_email }
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  end
+
+  context 'when registering' do
+    context 'a trendsetter' do
+      let     (:user){ FactoryGirl.create(:user) }
+      subject { user }
+
+      it { should be_persisted }
+    end
+
+    context 'a non-trendsetter' do
+      it 'can\'t register' do
+        expect {FactoryGirl.create(:user_non_trendsetter)}.to raise_error
+      end
+    end
+
+  end
 end
