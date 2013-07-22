13b49982b363e8f266ee7b4ac7e2e482140b123c
TSMMark <marka@trendsettermarketing.net>
2013-07-22 10:50:19 -0400

    network spec

--- spec/models/network_spec.rb
+++ spec/models/network_spec.rb
@@ -1,18 +1,22 @@
 require 'spec_helper'
 
 describe Network do
-  before do
-  end
+  let (:network)  { FactoryGirl.create :network_default }
 
-  context 'when creating' do
-
+  describe 'when creating' do
     it 'has a token with non-digits' do
       10.times do
-        n = FactoryGirl.create :network
+        n = FactoryGirl.create :network_default
         n.token.should_not be_numeric
       end
     end
-    
+  end
+
+  describe 'when finding' do
+    let (:id)     { network.id }
+    let (:token)  { network.token }
+    it { Network.find(token).should == network }
+    it { Network.find(id).should    == network }
   end
 
 end
